679d29de85bd9e239cd6d933b8459a39f3c45c99
shohrux <nurmatovshohruhbek1@gmail.com>
2023-08-01 08:36:18 +0500

    first commit

---
+++ js/shoh.js
@@ -0,0 +1,18 @@
+const KEY = '6988d0d0b69d81ebe313e54bf6120098'
+
+
+const getData = async (city)=>{
+    const base = 'https://api.openweathermap.org/data/2.5/weather'
+    const query = `?q=${city}&units=metric&appid=${KEY}`
+    loader(true)
+    const req = await fetch(base + query)
+    const data = await req.json()
+    loader(false)
+    return data
+
+}
+getData('London')
+.then((data)=>{
+    console.log(data)
+
+})
